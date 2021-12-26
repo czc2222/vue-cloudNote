f4b2893b9336c856fafec86fa296a6555a1baf56
czc222 <1263291450@qq.com>
2021-12-26 14:22:24 +0800

    first commit

---
+++ vue-evernote-client/src/components/Login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>这是login</div>
+</template>
+<script>
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ vue-evernote-client/src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/components/Login.vue'
+import NotebookList from '../components/NotebookList.vue'
+import NoteDetail from '../components/NoteDetail.vue'
+import TrashDetail from '../components/TrashDetail.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/notebooks',
+      component: NotebookList
+    },
+    {
+      path: '/note/:noteId',
+      component: NoteDetail
+    },
+    {
+      path: '/trash/:noteId',
+      component: TrashDetail
+    }
+
+  ]
+})
