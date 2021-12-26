62f655fa77f2ccb209831853069746ab91a8c699
czc222 <1263291450@qq.com>
2021-12-26 17:17:47 +0800

    创建头像组件，基本完成login的功能

--- vue-evernote-client/src/App.vue
+++ vue-evernote-client/src/App.vue
@@ -1,23 +1,63 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+
+       <SideBar></SideBar>
+       <router-view></router-view>
   </div>
 </template>
 
 <script>
+
+  import SideBar from '@/components/SideBar.vue'
+
 export default {
-  name: 'App'
+    name:'app',
+    components: {
+      SideBar
+    }
 }
 </script>
 
 <style>
-#app {
+@import '//at.alicdn.com/t/font_496303_kqrjhri8l25d0a4i.css';
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+html, body, #app {
+ height: 100%;
+}
+body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+  font-size: 14px;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #eee;
+}
+
+
+a {
+  text-decoration: none;
+  color: #444;
+}
+
+ul, li{
+  list-style: none;
+}
+
+.btn {
+  color: #666;
+  font-size: 12px;
+  padding: 2px 4px;+  background-color: #fff;
+  box-shadow: 0px 0px 2px 0px #ccc;
+  border: none;
+  cursor: pointer;
+  display: inline-block;
+}
+
+
+#app {
+  display: flex;
+  align-items: stretch;
 }
 </style>
--- vue-evernote-client/src/components/Login.vue
+++ vue-evernote-client/src/components/Login.vue
@@ -1,10 +1,213 @@
 <template>
-  <div>这是login</div>
+  <div id="login">
+
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="main"></div>
+          <div class="form">
+            <h3 @click="showRegister">创建账户</h3>
+            <div v-show="isShowRegister" class="register">
+              <input type="text" placeholder="用户名" v-model="register.username">
+              <input type="password" placeholder="密码" v-model="register.password">
+              <p v-bind:class="{error:register.isError}">{{register.notice}}</p>
+              <div class="button" @click="onRegister">创建账号</div>
+            </div>
+            <h3 @click="showLogin">登录</h3>
+            <div v-show="isShowLogin" class="login" >
+              <input type="text" placeholder="输入用户名" v-model="login.username">
+              <input type="password" placeholder="密码" v-model="login.password">
+              <p v-bind:class="{error:login.isError}">{{login.notice}}</p>
+              <div class="button" @click="onLogin"> 登录</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+export default {
+  data(){
+    return{
+      isShowLogin: true,
+      isShowRegister: false,
+      login:{
+        username:'hunger',
+        password:'123456',
+        notice:'请输入用户名和密码',
+        isError:false
+      },
+      register:{
+        username:'',
+        password:'',
+        notice:'创建账户后请记住你的用户名和密码',
+        isError:false
+      }
+    }
 
+  },
+  methods:{
+    showRegister(){
+      this.isShowRegister=true
+      this.isShowLogin=false
+    },
+    showLogin(){
+      this.isShowLogin =true
+      this.isShowRegister=false
+    },
+    onRegister(){
+          let result1=this.validUsername(this.register.username)
+      if(!result1.isValid){
+        this.register.isError=true
+        this.register.notice = result1.notice
+        return
+      }
+      let result2 =this.validPassword(this.register.password)
+      if(!result2.isValid){
+        this.register.isError =true
+        this.register.notice =result2.notice
+        return
+      }
+      this.register.isError =false
+      this.register.notice = '注册成功'
+      console.log('账户:'+this.register.username,'密码:'+this.register.password)
+    },
+    onLogin(){
+      let result1=this.validUsername(this.login.username)
+      if(!result1.isValid){
+        this.login.isError=true
+        this.login.notice = result1.notice
+        return
+      }
+      let result2 =this.validPassword(this.login.password)
+      if(!result2.isValid){
+        this.login.isError =true
+        this.login.notice =result2.notice
+        return
+      }
+      this.login.isError =false
+      this.login.notice = '登入成功'
+      console.log('开始登入:账户:'+this.login.username,'密码:'+this.login.password)
+    },
+    validUsername(username){
+      return{
+        isValid:/^[\w\u4e00-\u9fa5]{3,15}$/.test(username),
+        notice:'用户名3~15个字符，仅限于字母数字下划线中文'
+      }
+
+    },
+    validPassword(password){
+      return{
+        isValid:/^.{6,16}$/.test(password),
+        notice:'密码长度为6~16个字符'
+      }
+    }
+  }
+}
 </script>
 
-<style lang="css" scoped>
+<style lang="less">
+
+.modal-mask {
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .7);
+  display: table;
+  transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 800px;
+  height: 500px;
+  margin: 0px auto;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+  transition: all .3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+  display: flex;
+
+  .main {
+    flex: 1;
+    background: url(~@/assets/cloudNote.png) center center no-repeat;
+
+  }
+
+  .form {
+    width: 270px;
+    border-left: 1px solid #ccc;
+
+    h3 {
+      padding: 10px 20px;
+      font-weight: normal;
+      font-size: 16px;
+      border-top: 1px solid #eee;
+      cursor: pointer;
+
+      &:nth-of-type(2) {
+        border-bottom: 1px solid #eee;
+      }
+    }
+
+    .button {
+      background-color: #005ce6;
+      height: 36px;
+      line-height: 36px;
+      text-align: center;
+      font-weight: bold;
+      color: #fff;
+      border-radius: 4px;
+      margin-top: 18px;
+      cursor: pointer;
+    }
+
+    .login, .register {
+      padding: 10px 20px;
+      border-top: 1px solid #eee;
+
+      input {
+        display: block;
+        width: 100%;
+        height: 35px;
+        line-height: 35px;
+        padding: 0 6px;
+        border-radius: 4px;
+        border: 1px solid #ccc;
+        outline: none;
+        font-size: 14px;
+        margin-top: 10px;
+      }
+
+      input:focus {
+        border: 3px solid #9dcaf8;
+      }
+
+      p {
+        font-size: 12px;
+        margin-top: 10px;
+        color: #444;
+      }
+
+      .error {
+        color: red;
+      }
+    }
+
+    .login {
+      border-top: 0;
+    }
+  }
+}
 
 </style>
