6ddf486ac4e91046d9aed7a21ab497233a4f3afb
czc222 <1263291450@qq.com>
2021-12-27 13:54:06 +0800

    用axios跟promise 对网络请求进行处理

--- vue-evernote-client/src/components/Login.vue
+++ vue-evernote-client/src/components/Login.vue
@@ -32,6 +32,10 @@
   </div>
 </template>
 <script>
+import request from "../helpers/request";
+request('/auth/login','POST',{username:'hunger',password:'123456'}).then(data=>{
+  console.log(data);
+})
 export default {
   data() {
     return {
