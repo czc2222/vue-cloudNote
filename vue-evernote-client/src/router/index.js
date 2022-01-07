d5dc1bd894c419d0ec63de1fbd1bd05b1188fa77
czc222 <1263291450@qq.com>
2022-01-07 17:14:50 +0800

    解决vue-router 连续刷新报错问题

--- vue-evernote-client/src/router/index.js
+++ vue-evernote-client/src/router/index.js
@@ -1,36 +1,58 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
-import Login from '@/components/Login.vue'
-import NotebookList from '../components/NotebookList.vue'
-import NoteDetail from '../components/NoteDetail.vue'
-import TrashDetail from '../components/TrashDetail.vue'
 
 Vue.use(Router)
+
+// 先把VueRouter原型对象的push replace  方法保存一份
+let originPush=Router.prototype.push;
+let originReplace=Router.prototype.replace;
+// 重写push方法 第一个参数
+Router.prototype.push=function(location,resolve,reject){
+  // console.log(this);//实例
+  // 判断如果成功或失败回调已经传了
+  if(resolve&&reject){
+    // call 调用函数一次  改变this指向
+    originPush.call(this,location,resolve,reject)
+  }else{
+    originPush.call(this,location,()=>{},()=>{})
+  }
+}
+
+
+Router.prototype.replace=function(location,resolve,reject){
+  if(resolve&&reject){
+    originReplace.call(this,location,resolve,reject)
+  }else {
+    originReplace.call(this, location, () => {}, () => {})
+  }
+}
+
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      alias:'/notebooks',
+      component: () => import('@/components/NotebookList.vue') //懒加载
     },
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: () => import('@/components/Login.vue') //懒加载
+
     },
-    {
-      path: '/notebooks',
-      component: NotebookList
-    },
+    // {
+    //   path: '/notebooks',
+    //   component: NotebookList
+    // },
     {
       path: '/note',
-      component: NoteDetail
+      component: () => import('@/components/NoteDetail.vue') //懒加载
+
     },
     {
       path: '/trash',
-      component: TrashDetail
+      component: () => import('@/components/TrashDetail.vue') //懒加载
     }
 
   ]
